4e18f5e57e45af840abf3f5e59d1dc5ef651c8f6
LAPTOP-03GUROLK\ZNAKE28 <piyakorn.pa@ku.th>
2021-01-10 11:07:48 +0700

    change folder

---
+++ variables/mutiple_word_name.py
@@ -0,0 +1,8 @@
+# Camel Case (java scrip,node,react,go)
+myVariableName = "Piyakorn panthapin"
+
+# Pascal Case (java)
+MyVariableName = "Piyakorn panthapin"
+
+# Snake Case (คนนิยมใช้ใน python)
+my_variable_Name = "Piyakorn panthapin"
---
+++ variables/name.py
@@ -0,0 +1,17 @@
+#สามารถประกาศได้
+myvar = "bam"
+my_var = "bam"
+_my_var_ = "bam"
+myVar = "bam"
+MYVAR = "bam"
+myvar2 = "bam"
+#ตัวพิมพ์ใหญ่ทั้งหมดคือตัวแปรที่ไม่ควรเปลี่ยนแปลงค่า
+
+
+#ไม่สามารถประกาศได้
+2myvar = "bam"
+my-var = "bam"
+my var = "bam"
+
+
+
