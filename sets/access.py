e38a213766e0cc5aa07f8c99ce540cd37fefeaca
LAPTOP-03GUROLK\ZNAKE28 <piyakorn.pa@ku.th>
2021-01-17 09:54:02 +0700

    edit 4 files and add LIST 8 files

--- sets/access.py
+++ sets/access.py
@@ -4,8 +4,14 @@ thisset = {"apple", "banana", "cherry"}
 for x in thisset:
     print(x)
 
+# Output :
+# banana
+# cherry
+# apple
 
 # Ex2
 fruits = {"apple", "banana", "cherry"}
 if "apple" in fruits:
     print("Yes, apple is a fruit!")
+
+# Output : Yes, apple is a fruit!
--- sets/add.py
+++ sets/add.py
@@ -3,10 +3,12 @@ thisset = {"apple", "banana", "cherry"}
 thisset.add("orange")
 print(thisset)
 
-# Output : {'aaple', 'orange', 'cherry', 'banana'}
+# Output : {'apple', 'orange', 'cherry', 'banana'}
 
 # EX 2
 thisset = {"apple", "banana", "cherry"}
 tropical = {"pineapple", "mango", "papaya"}
 thisset.update(tropical)
 print(thisset)
+
+# Output : {'pineapple', 'apple', 'cherry', 'papaya', 'banana', 'mango'}
